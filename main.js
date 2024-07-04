8c87a6604ff0676f132601236d0cca5115112d78
kkokkomii <128340234+kkokkomii@users.noreply.github.com>
2024-07-05 01:57:18 +0900

    update sliding underline & tab

--- main.js
+++ main.js
@@ -6,17 +6,32 @@
 //완료 탭을 누르면 현제 체크가 된 할일한 선택되어 보여진다
 
 
-// let todo = document.getElementsByClassName("input_area")
 let todo_list = [];
-
 let TodoButton = document.getElementById("todo");
 let AddButton = document.getElementById("add");
 let tabs = document.querySelectorAll(".task_tabs div");
-console.log(tabs);
+let underline = document.getElementById("under_line");
+let mode = "all"
+let filteredList = []
 
+
+console.log(underline)
+//탭을 누르면 탭에 맞는 todo를 모아서 보여줌
 for(let i=1; i< tabs.length;i++){
   tabs[i].addEventListener("click", function(event){filter(event)});
 }
+
+
+// 엔터키로 Go 버튼 작동시키기
+TodoButton.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    list_plus(event);
+  }
+
+});
+
+
+// AddButton을 누르면, todo list에 Todo를 추가함
 AddButton.addEventListener("click", list_plus);
 
 function list_plus(){
@@ -25,29 +40,38 @@ function list_plus(){
     todo_input : TodoButton.value,
     isComplete : false
   }
-  todo_list.push(todo);
-  // const listItem = document.createElement("li");
-  // listItem.textContent = todo_input;
+  if (TodoButton.value === "" || TodoButton.value === "") {
+    alert("todo 내용을 입력해주세요!"); 
+    return;
+  }
 
-  // list.appendChild(listItem);
-  // inputField.value = "";
-  console.log(todo_list);
-  render(todo_list);
+  todo_list.push(todo);
+  TodoButton.value = "";
+
+  render();
 
 }
 
-function render(filteredList){
+// render함수로, Todolist에 있는 todo들을 뿌려줌. 여기서 check, undo, delete 버튼을 만들어줌
+function render(){
   let resultHTML = ''
-  for(let i=0;i<filteredList.length;i++){
-    if(filteredList[i].isComplete == true){
+  let list = []
+  if(mode === "all"){
+    list = todo_list
+  }else{
+    list = filteredList
+  }
+
+  for(let i=0;i<list.length;i++){
+    if(list[i].isComplete == true){
 
       resultHTML += `<div class = "task_list" style='background-color: #f3f0f0'>
-                <div class="task_done">${filteredList[i].todo_input}  </div>
+                <div class="task_done">${list[i].todo_input}  </div>
                   
                   <div> 
-                    <button onclick="togglecomplete('${filteredList[i].id}')" class="btn-hover color-9">
+                    <button onclick="togglecomplete('${list[i].id}')" class="btn-hover color-9">
                     undo</button>
-                    <button onclick="deleteTask('${filteredList[i].id}')" class="btn-hover color-10">
+                    <button onclick="deleteTask('${list[i].id}')" class="btn-hover color-10">
                     delete</button>
                   </div>
                 </div>
@@ -56,13 +80,13 @@ function render(filteredList){
 
     }else {
       resultHTML += `<div class = "task_list">
-                  <div>${filteredList[i].todo_input}  </div>
+                  <div>${list[i].todo_input}  </div>
                     
                     <div> 
-                    <button onclick="togglecomplete('${filteredList[i].id}')" class="btn-hover color-9">
+                    <button onclick="togglecomplete('${list[i].id}')" class="btn-hover color-9">
                     Check</button>
                     
-                    <button onclick="deleteTask('${filteredList[i].id}')" class="btn-hover color-10">
+                    <button onclick="deleteTask('${list[i].id}')" class="btn-hover color-10">
                     delete</button>
                     
                     </div>
@@ -80,27 +104,29 @@ function togglecomplete(id){
       break;
     }
   }
-  render(todo_list)
+  filter()
 }
 
 function deleteTask(id){
   for(let i=0; i<todo_list.length;i++){
     if(todo_list[i].id == id){
       todo_list.splice(i,1);
-      break;
     }
   }
-  render(todo_list)
+  filter()
 }
 
+
 function filter(event){
-  let mode = event.target.id 
-  let filteredList = []
-  if( mode == "all"){
-    //전체 리스트
- 
-    render(todo_list)
-  }else if(mode == "ongoing"){
+  if (event) {
+    mode = event.target.id 
+    underline.style.width = event.target.offsetWidth + "px";
+    underline.style.left = event.target.offsetLeft + "px"; 
+    underline.style.top =
+    event.target.offsetTop + (event.target.offsetHeight - 4) + "px";
+  }
+  filteredList = []
+  if(mode == "ongoing"){
     //진행중 아이템 보여줌
     //isComplete == false
     for(let i=0; i<todo_list.length;i++){
@@ -108,25 +134,25 @@ function filter(event){
         filteredList.push(todo_list[i])
       }
     }
-    render(filteredList)
-
   }else if(mode =="done"){
-    //isComplete == false
+    //isComplete == true
     for(let i=0; i<todo_list.length;i++){
       if(todo_list[i].isComplete == true){
         filteredList.push(todo_list[i])
       }
     }
-    render(filteredList)
   }
+  render()
 }
 
 
+tabs.forEach((menu) =>  menu.addEventListener("click", (e) => slide_indicator(e)));
 
-function slide(event){
-  resultHTML += `<div id="under_line"></div>`
-  document.getElementById("task-board").innerHTML= resultHTML;
-}
+// function slide_indicator(e){
+//   resultHTML += `<div id="under_line"></div>`
+//   document.getElementById("task-board").innerHTML= resultHTML;
+
+// }
 
 function randomIDGenerate(){
   return '_' + Math.random().toString(36).substr(2,9);
